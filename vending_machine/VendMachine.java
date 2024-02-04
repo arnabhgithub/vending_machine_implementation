f163790c3d4f18b7410abcfc07ead70ac7327bab
arnabhgithub <133379075+arnabhgithub@users.noreply.github.com>
2024-02-04 20:37:46 +0530

    Add files via upload

---
+++ vending_machine/VendMachine.java
@@ -0,0 +1,19 @@
+package vending_machine;
+
+import javax.swing.*; 
+
+public class VendMachine {
+
+	public static void main(String args[]) {
+
+		BuildMachine(); }
+
+	public static void BuildMachine() {
+
+		JFrame VendingMachine = new VMGUI();
+
+		VendingMachine.show();
+
+	}
+
+}
